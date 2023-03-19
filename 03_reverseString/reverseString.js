9bf332c6b61cc2f01f25453e58ae6fad1f409dc1
Quanmeo <huuquan.learn@gmail.com>
2023-03-19 16:23:44 +0700

    [TOP_js] Use split of string and then join to construct a reverse string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    const a = string.split('').reverse();
 
+    return a.join('');
 };
 
 // Do not edit below this line
