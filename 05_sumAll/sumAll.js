a802023f03e84a0f42b6b33a8003d0d01722777e
Quanmeo <huuquan.learn@gmail.com>
2023-03-19 17:01:03 +0700

    [TOP_js] Do 05, 06 exercises

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(lower, upper) {
+    if (!(Number.isInteger(lower) && Number.isInteger(upper)
+    && lower >= 0 && upper >= 0)) {
+        return 'ERROR';
+    }
 
+    const min = (lower < upper) ? lower : upper;
+    const max = (lower + upper) - min;
+
+    let ret = 0;
+
+    for (let i = min; i <= max; i++)
+    {
+        ret += i;
+    }
+
+    return ret;
 };
 
 // Do not edit below this line
