024dfee2b712c37a6f019512e459cfd6ab802f93
Quanmeo <huuquan.learn@gmail.com>
2023-03-19 16:10:28 +0700

    [TOP_js] Do 01 and 02 exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    if (num < 0) {
+        return 'ERROR';
+    } else if (string === '') {
+        return '';
+    }
 
+    let ret = '';
+    while (num-- > 0) {
+        ret += string;
+    }
+
+    return ret;
 };
 
 // Do not edit below this line
