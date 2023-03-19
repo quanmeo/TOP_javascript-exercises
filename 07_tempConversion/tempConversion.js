625355045881b98c45b2c6afce5a11a75085d2e4
Quanmeo <huuquan.learn@gmail.com>
2023-03-19 17:08:02 +0700

    [TOP_js] Do 07 excerise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,10 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  return Math.round((5 * (fahrenheit - 32) / 9) * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+
+  return Math.round((9 * (celsius) / 5 + 32) * 10) / 10;
 };
 
 // Do not edit below this line
