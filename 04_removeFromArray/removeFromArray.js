a7e6464a63c0867f074af199d100b35df519addf
Quanmeo <huuquan.learn@gmail.com>
2023-03-19 16:47:59 +0700

    [TOP_js] Do 04 exercises

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,21 @@
-const removeFromArray = function() {
+const removeFromArray = function(listItems, ...removedItems) {
+    let ret = [];
 
+    for (const item of listItems) {
+        let removed = false;
+        for (const i of removedItems) {
+            if (item === i) {
+                removed = true;
+                break;
+            }
+        }
+
+        if (!removed) {
+            ret.push(item);
+        }
+    }
+
+    return ret;
 };
 
 // Do not edit below this line
