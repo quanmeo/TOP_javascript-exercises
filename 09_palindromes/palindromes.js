69f6a94936f79f6532b19460f2cf020d9ed84393
Quanmeo <huuquan.learn@gmail.com>
2023-04-01 10:52:22 +0700

    [TOP_js] checking a palindromes string

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const reg = /[a-zA-Z]/
 
+    const arrTemp =  str.split('');
+
+    const arr = arrTemp.filter((c) => {
+        return c.search(reg) !== -1;
+    });
+
+    for (let i = 0, j = arr.length - 1; i < j; i++, j--) {
+        if (arr[i].toLowerCase() !== arr[j].toLowerCase()) {
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
