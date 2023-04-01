eeb346e73012ea91eb9c94d36107496b4e04a961
Quanmeo <huuquan.learn@gmail.com>
2023-04-01 11:34:33 +0700

    [TOP_js] use reduce

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
+const getAge = function(person) {
+    return (person.hasOwnProperty('yearOfDeath') ? (person['yearOfDeath'] - person['yearOfBirth']) : (new Date().getFullYear() - person['yearOfBirth']));
+};
 
+const findTheOldest = function(people) {
+    const oldest = people.reduce((acc, next) => {
+
+        return (getAge(acc) < getAge(next) ? next : acc);
+    }, {yearOfBirth: new Date().getFullYear()});
+
+    return oldest;
 };
 
 // Do not edit below this line
