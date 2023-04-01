8ed0162a4427c7f554ed8e0cf50b202f0a751b69
Quanmeo <huuquan.learn@gmail.com>
2023-04-01 11:05:53 +0700

    [TOP_js] Do fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if (num < 1) {
+        return 'OOPS';
+    }
 
+    let a = 0;
+    let b = 1;
+    for (let i = 1; i <= num; i++) {
+        [a, b] = [b, a + b];
+    }
+
+    return a;
 };
 
 // Do not edit below this line
